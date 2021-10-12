fb9ad3006058444b397b518180cc4fbdd5690b48
longdaungo <zedvidieu@gmail.com>
2021-10-12 10:28:17 +0700

    - add wallet and location screen

---
+++ bookingpitch5/lib/screen/home_screen/location/image_banner.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ImageBanner extends StatelessWidget{
+  final String _assetPath;
+  final Color _color;
+
+  ImageBanner(this._assetPath, this._color);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      constraints: BoxConstraints.expand(
+        height: 100.0,
+      ),
+      decoration: BoxDecoration(color: _color),
+      child: Image.asset(_assetPath),
+    );
+  }
+
+}
